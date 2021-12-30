c2e7fff60ce36937003cba64da09238b5500a5ac
deforde <daniel.forde001@gmail.com>
2021-12-30 10:23:35 +0200

    Complete day 7.

--- src/aoc_2020/day_07.c
+++ src/aoc_2020/day_07.c
@@ -22,7 +22,7 @@ typedef struct {
     size_t n_contained_bags;
 } bag_rule_t;
 
-static size_t solve_part_1(const char* const input, size_t size)
+static void extract_rules(const char* const input, size_t size, bag_rule_t** output, size_t* output_len)
 {
     memory_reference_t* line_mem_ref = NULL;
     size_t n_lines = 0;
@@ -47,7 +47,6 @@ static size_t solve_part_1(const char* const input, size_t size)
         char* container_bag_str = malloc(container_bag_len + 1);
         container_bag_str[container_bag_len] = 0;
         memcpy(container_bag_str, container_bag ,container_bag_len);
-        //printf("container_bag_str = %s\n", container_bag_str);
         rules[line_idx].bag_type = container_bag_str;
 
         const char* const contained_bags_list = &line[line_segment_mem_ref[1].offset];
@@ -76,22 +75,17 @@ static size_t solve_part_1(const char* const input, size_t size)
             char* num_str = malloc(contained_bag_portions_mem_ref[0].size + 1);
             num_str[contained_bag_portions_mem_ref[0].size] = 0;
             memcpy(num_str, &contained_bag[contained_bag_portions_mem_ref[0].offset], contained_bag_portions_mem_ref[0].size);
-            //printf("num_str = %s\n", num_str);
 
             char* bag_str = malloc(contained_bag_portions_mem_ref[1].size + contained_bag_portions_mem_ref[2].size + 2);
             bag_str[contained_bag_portions_mem_ref[1].size + contained_bag_portions_mem_ref[2].size + 1] = 0;
             memcpy(bag_str, &contained_bag[contained_bag_portions_mem_ref[1].offset], contained_bag_portions_mem_ref[1].size + contained_bag_portions_mem_ref[2].size + 1);
-            //printf("bag_str = %s\n", bag_str);
 
             rules[line_idx].contained_bags[contained_bag_idx].bag_type = bag_str;
             rules[line_idx].contained_bags[contained_bag_idx].count = atoi(num_str);
 
-            //free(bag_str);
             free(num_str);
             free(contained_bag_portions_mem_ref);
         }
-
-        //free(container_bag_str);
 
         free(contained_bags_mem_ref);
         free(line_segment_mem_ref);
@@ -99,65 +93,12 @@ static size_t solve_part_1(const char* const input, size_t size)
 
     free(line_mem_ref);
 
-    char* initial_bag = "shiny gold";
-    size_t initial_bag_index = SIZE_MAX;
-    for(size_t i = 0; i < n_rules; i++) {
-        bag_rule_t* rule = &rules[i];
-        if(strcmp(rule->bag_type, initial_bag) == 0) {
-            initial_bag_index = i;
-            break;
-        }
-    }
-    assert(initial_bag_index != SIZE_MAX);
+    *output = rules;
+    *output_len = n_rules;
+}
 
-    size_t* search_rules = (size_t*)malloc(n_rules * sizeof(size_t));
-    search_rules[0] = initial_bag_index;
-    size_t n_search_rules = 1;
-
-    size_t* searched_rules = (size_t*)malloc(n_rules * sizeof(size_t));
-    searched_rules[0] = initial_bag_index;
-    size_t n_searched_rules = 1;
-
-    size_t container_bag_count = 0;
-    while(n_search_rules > 0) {
-        size_t* temp_search_rules = (size_t*)malloc(n_rules * sizeof(size_t));
-        size_t n_temp_search_rules = 0;
-
-        for(size_t k = 0; k < n_search_rules; k++) {
-            bag_rule_t* search_rule = &rules[search_rules[k]];
-
-            for(size_t i = 0; i < n_rules; i++) {
-                bag_rule_t* rule = &rules[i];
-                for(size_t j = 0; j < rule->n_contained_bags; j++) {
-                    bag_count_t* contained_bag = &rule->contained_bags[j];
-                    if(strcmp(search_rule->bag_type, contained_bag->bag_type) == 0) {
-                        bool new_index = true;
-                        for(size_t h = 0; h < n_searched_rules; h++) {
-                            if(searched_rules[h] == i) {
-                                new_index = false;
-                                break;
-                            }
-                        }
-                        if(new_index) {
-                            searched_rules[n_searched_rules++] = i;
-                            //printf("search_rule = %s, contained_bag = %s, container_bag = %s\n", search_rule->bag_type, contained_bag->bag_type, rule->bag_type);
-                            temp_search_rules[n_temp_search_rules++] = i;
-                            container_bag_count++;
-                        }
-                        break;
-                    }
-                }
-            }
-        }
-
-        free(search_rules);
-        search_rules = temp_search_rules;
-        n_search_rules = n_temp_search_rules;
-    }
-
-    free(searched_rules);
-    free(search_rules);
-
+static void destroy_bag_rules(bag_rule_t* rules, size_t n_rules)
+{
     for(size_t i = 0; i < n_rules; i++) {
         free(rules[i].bag_type);
         for(size_t j = 0; j < rules[i].n_contained_bags; j++) {
@@ -166,8 +107,76 @@ static size_t solve_part_1(const char* const input, size_t size)
         free(rules[i].contained_bags);
     }
     free(rules);
+}
 
-    return container_bag_count;
+static void find_all_container_bags(bag_rule_t* rules, size_t n_rules, const char* const bag_type, size_t* container_bag_indices, size_t* n_containter_bag_indices)
+{
+    for(size_t i = 0; i < n_rules; i++) {
+        bag_rule_t* rule = &rules[i];
+        for(size_t j = 0; j < rule->n_contained_bags; j++) {
+            bag_count_t* contained_bag = &rule->contained_bags[j];
+            if(strcmp(bag_type, contained_bag->bag_type) == 0) {
+                bool new_index = true;
+                for(size_t k = 0; k < *n_containter_bag_indices; k++) {
+                    if(container_bag_indices[k] == i) {
+                        new_index = false;
+                        break;
+                    }
+                }
+                if(new_index) {
+                    container_bag_indices[*n_containter_bag_indices] = i;
+                    *n_containter_bag_indices += 1;
+
+                    find_all_container_bags(rules, n_rules, rule->bag_type, container_bag_indices, n_containter_bag_indices);
+                }
+            }
+        }
+    }
+}
+
+static size_t solve_part_1(const char* const input, size_t size)
+{
+    bag_rule_t* rules = NULL;
+    size_t n_rules = 0;
+    extract_rules(input, size, &rules, &n_rules);
+
+    size_t* container_bag_indices = (size_t*)malloc(n_rules * sizeof(size_t));
+    size_t n_container_bag_indices = 0;
+    find_all_container_bags(rules, n_rules, "shiny gold", container_bag_indices, &n_container_bag_indices);
+
+    free(container_bag_indices);
+
+    destroy_bag_rules(rules, n_rules);
+
+    return n_container_bag_indices;
+}
+
+static size_t recursive_contained_bag_count(bag_rule_t* rules, size_t n_rules, const char* const bag_type)
+{
+    size_t count = 1;
+    for(size_t i = 0; i < n_rules; i++) {
+        bag_rule_t* rule = &rules[i];
+        if(strcmp(bag_type, rule->bag_type) == 0) {
+            for(size_t j = 0; j < rule->n_contained_bags; j++) {
+                bag_count_t* contained_bag = &rule->contained_bags[j];
+                count += contained_bag->count * recursive_contained_bag_count(rules, n_rules, contained_bag->bag_type);
+            }
+        }
+    }
+    return count;
+}
+
+static size_t solve_part_2(const char* const input, size_t size)
+{
+    bag_rule_t* rules = NULL;
+    size_t n_rules = 0;
+    extract_rules(input, size, &rules, &n_rules);
+
+    const size_t total_contained_bag_count = recursive_contained_bag_count(rules, n_rules, "shiny gold") - 1;
+
+    destroy_bag_rules(rules, n_rules);
+
+    return total_contained_bag_count;
 }
 
 void day_07_part_1_example()
@@ -202,10 +211,30 @@ void day_07_part_1_problem()
 
 void day_07_part_2_example()
 {
+    char* input = NULL;
+    size_t size = 0;
 
+    const bool success = read_file_into_buf("../data/day_07_part_1_example.txt", &input, &size);
+    assert(success);
+
+    const uint64_t ans = solve_part_2(input, size);
+
+    free(input);
+
+    TEST_ASSERT_EQUAL_UINT64(32, ans);
 }
 
 void day_07_part_2_problem()
 {
+    char* input = NULL;
+    size_t size = 0;
 
+    const bool success = read_file_into_buf("../data/day_07_part_1_input.txt", &input, &size);
+    assert(success);
+
+    const uint64_t ans = solve_part_2(input, size);
+
+    free(input);
+
+    TEST_ASSERT_EQUAL_UINT64(12128, ans);
 }
