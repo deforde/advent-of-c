67e43fba5537d14187cc7d3a13c052813efa38c2
deforde <daniel.forde001@gmail.com>
2022-04-18 12:21:38 +0200

    WIP.

--- src/aoc_2020/day_23.c
+++ src/aoc_2020/day_23.c
@@ -5,118 +5,102 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unity.h>
 
 #include "../utility.h"
 
-#define CUP_SET_LEN 10
+#define NUM_CUPS 9
+#define SELECTED_CUPS_PER_MOVE 3
 
-static void process_input(const char* const input, size_t size, size_t cups[CUP_SET_LEN], size_t* n_cups)
+typedef struct cup_t {
+    size_t val;
+    struct cup_t* next;
+} cup_t;
+
+static void process_input(const char* const input, size_t size, cup_t* cups, size_t* first_cup_val)
 {
+    cup_t* prev_cup = NULL;
+    cup_t* first_cup = NULL;
     for(size_t i = 0; i < size; ++i) {
         if(isspace(input[i]) != 0) {
             break;
         }
         char temp[2] = {0};
         temp[0] = input[i];
-        cups[(*n_cups)++] = atoi(temp);
+        const size_t val = atoi(temp);
+        if(i == 0) {
+            first_cup = &cups[val - 1];
+            *first_cup_val = val;
+        }
+        if(prev_cup) {
+            prev_cup->next = &cups[val - 1];
+        }
+        prev_cup = &cups[val - 1];
+    }
+    prev_cup->next = first_cup;
+}
+
+static void play_game(cup_t* cups, size_t n_cups, size_t first_cup_val, size_t n_moves)
+{
+    cup_t* current_cup = &cups[first_cup_val - 1];
+    for(size_t i = 0; i < n_moves; ++i) {
+        cup_t* selected_cup = current_cup->next;
+        cup_t* new_current_cup_connect = selected_cup;
+        for(size_t j = 0; j < SELECTED_CUPS_PER_MOVE; ++j) {
+            new_current_cup_connect = new_current_cup_connect->next;
+        }
+        current_cup->next = new_current_cup_connect;
+
+        size_t destination_cup_val = current_cup->val;
+        bool destination_cup_in_selection = false;
+        do {
+            destination_cup_val--;
+            if(destination_cup_val == 0) {
+                destination_cup_val = n_cups;
+            }
+            destination_cup_in_selection = false;
+            cup_t* this_selected_cup = selected_cup;
+            for(size_t j = 0; j < SELECTED_CUPS_PER_MOVE; ++j) {
+                if(destination_cup_val == this_selected_cup->val) {
+                    destination_cup_in_selection = true;
+                    break;
+                }
+                this_selected_cup = this_selected_cup->next;
+            }
+        } while(destination_cup_in_selection);
+        cup_t* destination_cup = &cups[destination_cup_val - 1];
+
+        cup_t* selection_end_connect = destination_cup->next;
+        destination_cup->next = selected_cup;
+        cup_t* selection_end_cup = selected_cup;
+        for(size_t j = 0; j < SELECTED_CUPS_PER_MOVE - 1; ++j) {
+            selection_end_cup = selection_end_cup->next;
+        }
+        selection_end_cup->next = selection_end_connect;
+
+        current_cup = new_current_cup_connect;
     }
 }
 
 static size_t solve_part_1(const char* const input, size_t size, size_t n_moves)
 {
-    size_t cups[CUP_SET_LEN] = {0};
-    size_t n_cups = 0;
-    process_input(input, size, cups, &n_cups);
-    const size_t total_num_cups = n_cups;
-
-    size_t current_cup_idx = 0;
-    const size_t n_cups_selected_per_move = 3;
-    for(size_t i = 0; i < n_moves; ++i) {
-        // printf("Move #%lu\n", i + 1);
-        // printf("cups: ");
-        // for(size_t j = 0; j < n_cups; ++j) {
-        //     printf("%lu ", cups[j]);
-        // }
-        // printf("\n");
-
-        size_t selected_cups[CUP_SET_LEN] = {0};
-        size_t n_selected_cups = 0;
-
-        size_t selected_cup_idx = (current_cup_idx + 1) % n_cups;
-        for(size_t j = 0; j < n_cups_selected_per_move; ++j) {
-            selected_cups[n_selected_cups++] = cups[selected_cup_idx];
-            if(selected_cup_idx != n_cups - 1) {
-                memmove(&cups[selected_cup_idx],
-                        &cups[selected_cup_idx + 1],
-                        (n_cups - selected_cup_idx - 1) * sizeof(size_t));
-                if(selected_cup_idx < current_cup_idx) {
-                    current_cup_idx--;
-                }
-            }
-            n_cups--;
-            selected_cup_idx %= n_cups;
-        }
-
-        const size_t next_cup = cups[(current_cup_idx + 1) % n_cups];
-
-        size_t destination_cup = cups[current_cup_idx];
-        bool destination_cup_in_selection = false;
-        do {
-            destination_cup--;
-            if(destination_cup == 0) {
-                destination_cup = 9;
-            }
-            destination_cup_in_selection = false;
-            for(size_t j = 0; j < n_selected_cups; ++j) {
-                if(destination_cup == selected_cups[j]) {
-                    destination_cup_in_selection = true;
-                    break;
-                }
-            }
-        } while(destination_cup_in_selection);
-
-        size_t insertion_idx = 0;
-        for(size_t j = 0; j < n_cups; ++j) {
-            if(cups[j] == destination_cup) {
-                insertion_idx = (j + 1) % total_num_cups;
-                break;
-            }
-        }
-        if(insertion_idx != (total_num_cups - n_selected_cups)) {
-            memmove(&cups[insertion_idx + n_selected_cups], &cups[insertion_idx], (n_cups - insertion_idx) * sizeof(size_t));
-        }
-        memcpy(&cups[insertion_idx], selected_cups, n_selected_cups * sizeof(size_t));
-        n_cups += n_selected_cups;
-
-        for(size_t j = 0; j < n_cups; ++j) {
-            if(cups[j] == next_cup) {
-                current_cup_idx = j;
-                break;
-            }
-        }
+    cup_t cups[NUM_CUPS] = {0};
+    for(size_t i = 0; i < NUM_CUPS; ++i) {
+        cups[i].val = i + 1;
     }
+    size_t first_cup_val = 0;
+    process_input(input, size, cups, &first_cup_val);
 
-    // printf("cups: ");
-    // for(size_t j = 0; j < n_cups; ++j) {
-    //     printf("%lu ", cups[j]);
-    // }
-    // printf("\n");
+    play_game(cups, NUM_CUPS, first_cup_val, n_moves);
 
     size_t ans = 0;
-    size_t cup_one_idx = 0;
-    for(size_t i = 0; i < n_cups; ++i) {
-        if(cups[i] == 1) {
-            cup_one_idx = i;
-            break;
-        }
-    }
-    for(size_t i = 0; i < (n_cups - 1); ++i) {
+    cup_t* cup = cups[0].next;
+    for(size_t i = 0; i < (NUM_CUPS - 1); ++i) {
         ans *= 10;
-        ans += cups[(i + cup_one_idx + 1) % n_cups];
+        ans += cup->val;
+        cup = cup->next;
     }
-    // printf("ans = %lu\n", ans);
-
     return ans;
 }
 
@@ -163,5 +147,20 @@ void day_23_part_1_problem()
     free(input);
 
     TEST_ASSERT_EQUAL_size_t(27956483, ans);
+}
+
+void day_23_part_2_example()
+{
+    char* input = NULL;
+    size_t size = 0;
+
+    const bool success = read_file_into_buf("../data/day_23_part_1_example.txt", &input, &size);
+    TEST_ASSERT_TRUE(success);
+
+    const size_t ans = solve_part_1(input, size, 100);
+
+    free(input);
+
+    TEST_ASSERT_EQUAL_size_t(149245887792, ans);
 }
 
